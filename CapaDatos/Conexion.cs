b75a7b850edffcbac5e122a1e7ed3a678b0517ad
Itamar Medrano <martinezitamar777@gmail.com>
2026-01-31 19:18:49 -0600

    Subiendo Proyecto Ncapas correjido

--- CapaDatos/Conexion.cs
+++ CapaDatos/Conexion.cs
@@ -1,7 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
+using System.Data;
 using System.Data.SqlClient;
-using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -9,8 +8,8 @@ namespace CapaDatos
 {
     public class Conexion
     {
-        private static string cadena =
-            "Data Source=DEVELOPER;Initial Catalog=LoginDB;Integrated Security=True";
+        // Cadena de conexión completa en una sola línea
+        private static string cadena = "Data Source=localhost;Initial Catalog=SistemaLogin;Integrated Security=True;Persist Security Info=False;Pooling=False;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=True";
 
         public static SqlConnection obtenerConexion()
         {
